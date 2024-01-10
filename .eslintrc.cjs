6b560c5f867a56ba3968dd001b7296fa0a2069d7
Fredx87 <freddy87@gmail.com>
2024-01-11 00:14:19 +0100

    Repo setup

---
+++ .eslintrc.cjs
@@ -0,0 +1,12 @@
+/* eslint-env node */
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  ignorePatterns: ["dist/*"],
+  root: true,
+};
